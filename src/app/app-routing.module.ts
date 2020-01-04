4f254a31faffc94b08bfcf5de4e7c25df5fe9839
Rodrigo Campero <rodrigocampero.it@gmail.com>
2020-01-04 15:40:10 -0300

    added new component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DocumentFormComponent } from './document-form/document-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'docs', component: DocumentFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DocumentFormComponent } from './document-form/document-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocumentFormComponent
   ],
   imports: [
     BrowserModule,
